5e3cff775d5a48276838737b5a4fdfd062b48304
Bohdan Romashchenko <bohdan.romashchenko@ee.abb.com>
2020-01-07 19:12:13 +0200

    upd chat

--- src/Components/ChatWindow/ChatWindow.js
+++ src/Components/ChatWindow/ChatWindow.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Input from "../InputField/Input";
+import './Chat.css';
 import { ThemeProvider,MessageGroup,MessageList,MessageMedia,Message,MessageText,MessageTitle,MessageButtons,Row}
     from '@livechat/ui-kit';
+
 
 function ChatWindow({chatData}) {
     return (
         <ThemeProvider>
-            <MessageGroup
+            <MessageGroup id = 'group'
                 avatarLetter={chatData.name.charAt(0)}
-                onlyFirstWithMeta
             >
-                <Message authorName={chatData.name} date="21:37">
-                    <MessageText>Hey my friend!</MessageText>
+                <Message authorName={chatData.name} date="21:37" deliveryStatus={'seen'}>
+                    <MessageText>HBye</MessageText>
                 </Message>
                 <Message authorName={chatData.name} date="21:37">
                     <MessageText>Hi!</MessageText>
+                </Message>
+                <Message authorName={chatData.name} date="21:37">
+                    <MessageText>Hello, are you there?</MessageText>
+                </Message>
+                <Message authorName='Me' date="21:37" isOwn={true}>
+                    <MessageText>Yes blin</MessageText>
                 </Message>
                 <Message authorName={chatData.name} date="21:37">
                     <MessageText>Hello, are you there?</MessageText>
