6238f53a3d9f7315fc0e52b63ee79ea465e7ab6b
Bohdan <bohdan.romashchenko@gmail.com>
2020-01-09 02:12:26 +0200

    Updates

---
+++ precache-manifest.409c750d77cb84e18a32368348cdfc41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca939a09406ee34b5fd07fc1a9aa06ff",
+    "url": "/rocket-chat-front/index.html"
+  },
+  {
+    "revision": "5250359db812720abc10",
+    "url": "/rocket-chat-front/static/css/2.14482c78.chunk.css"
+  },
+  {
+    "revision": "2df9dd852c6519f7bb43",
+    "url": "/rocket-chat-front/static/css/main.f78b92b4.chunk.css"
+  },
+  {
+    "revision": "5250359db812720abc10",
+    "url": "/rocket-chat-front/static/js/2.3c12a5fe.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/rocket-chat-front/static/js/2.3c12a5fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2df9dd852c6519f7bb43",
+    "url": "/rocket-chat-front/static/js/main.948e804b.chunk.js"
+  },
+  {
+    "revision": "cb559ee10357bbe56cc5",
+    "url": "/rocket-chat-front/static/js/runtime-main.13f1949b.js"
+  },
+  {
+    "revision": "f8b7a2e44b33056a3282f4c7d74dbae2",
+    "url": "/rocket-chat-front/static/media/lol.f8b7a2e4.mp3"
+  }
+]);
