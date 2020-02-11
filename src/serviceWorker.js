241297137aebca0a1725e6de27a626cd3b516955
Bohdan <bohdan.romashchenko@gmail.com>
2020-02-11 21:37:04 +0200

    revert service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -72,7 +72,7 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              swal("New update!", `Update was downloaded, close all tabs to update!`, "success");
+              swal("New update!", `Rocket chat ${RELEASEDATA[0].version} was downloaded, close all tabs to update!`, "success");
 
               console.log(
                 'New content is available and will be used when all ' +
