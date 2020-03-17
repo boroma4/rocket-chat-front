8834997992cfb54e3c0aad6931c37e321bbd6284
Bohdan <bohdan.romashchenko@gmail.com>
2020-03-17 02:52:14 +0200

    Updated scrolls in chat window
    (cherry picked from commit 2848c67bb19d0169b9c38516a55157c915bf32c2)

--- src/Helpers/Scroller.js
+++ src/Helpers/Scroller.js
@@ -5,3 +5,13 @@ export const ScrollChatToBottom = () =>{
     const maxScrollTop = scrollHeight - height;
     chatWindow.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
 };
+
+export const CalculateScrollDistance  =  ()  =>  {
+    const win = document.getElementsByClassName("chat-panel")[0];
+    let h = win,
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+
+    return ((h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100);
+};
