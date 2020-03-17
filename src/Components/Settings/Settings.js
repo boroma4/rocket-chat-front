84496254cb97a12e3bbb5c3fd25d9cf9b30580b6
Bohdan <bohdan.romashchenko@gmail.com>
2020-03-17 04:10:52 +0200

    Add name in the main menu

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -2,8 +2,9 @@ import React,{useState} from 'react';
 import Dropdown from "react-bootstrap/Dropdown";
 import AudioSettingsModal from "./Modals/AudioSettingsModal";
 import AddContactModal from "./Modals/AddContactModal";
-import {SettingsList} from "../../Constants/Const";
+import {RELEASEDATA, SettingsList} from "../../Constants/Const";
 import MyAccountModal from "./Modals/MyAccountModal";
+import './Settings.css';
 
 
 
@@ -15,12 +16,14 @@ function Settings({updateAudio,logout,chooseSong,createNewChat,UpdateUserData}) 
 
     return (
         <>
-        <Dropdown>
+        <Dropdown className={'settings-ext'}>
             <Dropdown.Toggle variant="secondary" size={'sm'} id="dropdown-basic">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/>
                 <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
                 </svg>
             </Dropdown.Toggle>
+            <span className={'app-name'} > Rocket chat {RELEASEDATA[0].version}</span>
+
             <Dropdown.Menu>
                 {SettingsList.map((item,index)=>(
                     (item ==='Logout')
