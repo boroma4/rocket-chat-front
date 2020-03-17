55a303ba8b508b9a6920da8701bfa87ceed04d76
Bohdan <bohdan.romashchenko@gmail.com>
2020-03-17 03:17:23 +0200

    fix ui updates

--- src/Components/ChatWindow/ChatWindow.js
+++ src/Components/ChatWindow/ChatWindow.js
@@ -69,8 +69,8 @@ function ChatWindow({chatData,onSend,loadTen,GoBack}) {
                     </ChatPanel>
                 }
                 {(scrollPercentage <= 55)
-                    ?<div onClick={()=> ScrollChatToBottom()}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" className='down-btn'>
+                    ?<div>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" className='down-btn' onClick={()=> ScrollChatToBottom()}>
                             <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/>
                             <path d="M0 0h24v24H0V0z" fill="none"/>
                         </svg>
